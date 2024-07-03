50785c9d500adf882096a9e12cf27e6a6ce6c41e
valentinciolofan <valentincatalin02@gmail.com>
2024-07-03 20:33:46 +0300

    .

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,94 +1,46 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout
-} from '@stripe/react-stripe-js';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-// This is your test secret API key.
 const stripePromise = loadStripe("pk_test_51PVVxaEZbF6dio7iTjo5JguBuXPvFmlSfPJrZUkzEaksQnUuVP9nt3z74pjCzpI6O6qu3KuwSRYAK3N2Ft5xzIy700JPHkXNap");
 
-const CheckoutForm = () => {
-  const fetchClientSecret = useCallback(() => {
-    // Create a Checkout Session
-    return fetch("http://localhost:3000/create-checkout-session", {
+const CheckoutButton = ({ cartItems}) => {
+  console.log(cartItems);
+  const handleCheckout = useCallback(async () => {
+    const stripe = await stripePromise;
+    
+    const response = await fetch("http://localhost:3000/create-checkout-session", {
       method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => data.clientSecret);
-  }, []);
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(cartItems),
+    });
 
-  const options = { fetchClientSecret };
+    const session = await response.json();
+
+    // Redirect to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.error(result.error.message);
+    }
+  }, []);
 
   return (
-    <div id="checkout">
-      <EmbeddedCheckoutProvider
-        stripe={stripePromise}
-        options={options}
-      >
-        <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
-    </div>
-  )
-}
+    <button onClick={handleCheckout} className='btn-checkout'>
+      Checkout
+    </button>
+  );
+};
 
-const Return = () => {
-  const [status, setStatus] = useState(null);
-  const [customerEmail, setCustomerEmail] = useState('');
-
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const sessionId = urlParams.get('session_id');
-
-    fetch(`http://localhost:3000/session-status?session_id=${sessionId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setStatus(data.status);
-        setCustomerEmail(data.customer_email);
-      });
-  }, []);
-
-  if (status === 'open') {
-    return (
-      <Navigate to="/checkout" />
-    )
-  }
-
-  if (status === 'complete') {
-    return (
-      <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to {customerEmail}.
-
-          If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
-      </section>
-    )
-  }
-
-  return null;
-}
-
-const Payment = () => {
+const Payment = ({ cartItems }) => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/checkout" element={<CheckoutForm />} />
-          <Route path="/return" element={<Return />} />
-        </Routes>
-      </Router>
+      <CheckoutButton cartItems={cartItems} />
     </div>
-  )
-}
+  );
+};
 
 export default Payment;
