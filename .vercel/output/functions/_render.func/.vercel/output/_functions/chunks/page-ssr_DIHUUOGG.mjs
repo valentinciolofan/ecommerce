a788634b43c0dd8aa64297bd55d93a1736da289b
valentinciolofan <valentincatalin02@gmail.com>
2024-09-21 21:42:02 +0300

    .

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/page-ssr_DIHUUOGG.mjs
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"v2023-08-24","projectId":"1ymeft4k","dataset":"production","useCdn":false,"studioBasePath":"/admin"}
-          );
-
-globalThis.sanityClient = sanityClient;
-
-export { sanityClient as s };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/urlFor_D6lAtocX.mjs
+++
@@ -1,9 +0,0 @@
-import { s as sanityClient } from './page-ssr_DIHUUOGG.mjs';
-import imageUrlBuilder from '@sanity/image-url';
-
-const builder = imageUrlBuilder(sanityClient);
-function urlFor(source) {
-    return builder.image(source);
-}
-
-export { urlFor as u };
